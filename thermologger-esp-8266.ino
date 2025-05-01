ac9c5ef7f33560cb9a5734870b917d4e1e9bf186
Md. Hafizur Rahman <77488656+hafizurfpbd@users.noreply.github.com>
2025-05-01 21:58:51 +0600

    Update thermologger-esp-8266.ino

--- thermologger-esp-8266.ino
+++ thermologger-esp-8266.ino
@@ -1,1 +1,34 @@
+#include <ESP8266WiFi.h>
 
+const char* ssid = "IoT-WiFI";
+const char* password = "********";
+const char* host = "thermal-iot.bhuiyansoft.com";
+const char* api="********";
+
+void setup() {
+  Serial.begin(9600); // Communicate with Arduino Nano
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void loop() {
+  if (Serial.available()) {
+    String data = Serial.readStringUntil('\n');
+
+    WiFiClient client;
+    if (client.connect(host, 80)) {
+      String url = "/senddata/home/" + String(api) + "/" + String(data);
+      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                   "Host: " + host + "\r\n" +
+                   "Connection: close\r\n\r\n");
+    }
+    client.stop();
+    delay(16000); // Match ThingSpeak update rate
+  }
+}
+
+
+//https://thermal-iot.bhuiyansoft.com/senddata/home/machine_id/data
