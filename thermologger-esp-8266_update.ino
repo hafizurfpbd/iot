1c223a5ce6d230fcbe5eca9ac8a2212f6523e5d0
Md. Hafizur Rahman <77488656+hafizurfpbd@users.noreply.github.com>
2025-05-02 09:36:21 +0600

    Create thermologger-esp-8266_update.ino

---
+++ thermologger-esp-8266_update.ino
@@ -0,0 +1,49 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid = "NGIT_2G";
+const char* password = "NGIT334733";
+const char* host = "https://thermal-iot.bhuiyansoft.com/senddata/home/334733";
+const char* api = "334733";
+
+unsigned long lasttime = 0;
+const unsigned long interval = 10000;
+
+void setup() {
+  Serial.begin(9600);
+  delay(100);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void loop() {
+  if (Serial.available()) {
+    String data = Serial.readStringUntil('E');
+    unsigned long currenttime = millis();
+    if (currenttime - lasttime >= interval) {
+      lasttime = currenttime;
+
+      if (WiFi.status() == WL_CONNECTED) {
+        WiFiClientSecure client;
+        client.setInsecure();
+        HTTPClient http;
+
+        http.begin(client, host+"/" + api +"/" + data);
+        int httpCode = http.GET();
+        if (httpCode > 0) {
+          String response = http.getString();
+          Serial.println("Response: " + response);
+        } else {
+          Serial.println("GET failed, error: " + http.errorToString(httpCode));
+        }
+        http.end();
+
+      } else {
+        WiFi.begin(ssid, password);
+      }
+    }
+  }
+}
